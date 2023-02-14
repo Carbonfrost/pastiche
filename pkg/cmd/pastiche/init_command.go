b6f76489cbe48ce96097c5e9abc2cb0cf008341c
Gary Mann <garythemann@users.noreply.github.com>
2023-02-13 22:49:17 -0800

    Port init command to use templates

--- pkg/cmd/pastiche/init_command.go
+++ pkg/cmd/pastiche/init_command.go
@@ -1,12 +1,7 @@
 package pastiche
 
 import (
-	"encoding/json"
-	"os"
-	"path/filepath"
-
 	"github.com/Carbonfrost/joe-cli"
-	"github.com/Carbonfrost/pastiche/pkg/config"
 )
 
 func InitCommand() cli.Action {
@@ -26,50 +21,7 @@ func InitCommand() cli.Action {
 			},
 		}...)),
 		Action: func(c *cli.Context) error {
-			name := c.String("name")
-			if name == "" {
-				name = "service"
-				wd, err := os.Getwd()
-				if err == nil {
-					name = filepath.Base(wd)
-				}
-			}
-
-			svc := &config.Service{
-				Title:       c.String("title"),
-				Name:        name,
-				Description: c.String("description"),
-				Servers: []*config.Server{
-					{
-						Name:    "default",
-						BaseURL: "http://localhost:8000/",
-					},
-				},
-				Resource: &config.Resource{
-					Name: "/",
-					Resources: []*config.Resource{
-						{
-							Name: "get",
-							URI:  "/",
-							Get:  &config.Endpoint{},
-						},
-					},
-				},
-			}
-
-			_ = os.Mkdir(".pastiche", 0755)
-			f, err := os.Create(".pastiche/" + name + ".json")
-			if err != nil {
-				return err
-			}
-
-			b, err := json.MarshalIndent(svc, "", "  ")
-			if err != nil {
-				return err
-			}
-
-			_, err = f.Write(b)
-			return err
+			return c.Do(initTemplate(c))
 		},
 	}
 }
---
+++ pkg/cmd/pastiche/templates.go
@@ -0,0 +1,58 @@
+package pastiche
+
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/Carbonfrost/joe-cli"
+	. "github.com/Carbonfrost/joe-cli/extensions/template"
+	"github.com/Carbonfrost/pastiche/pkg/config"
+)
+
+func initTemplate(c *cli.Context) *Root {
+	svc := service(c)
+	return New(
+		Dir(".pastiche",
+			Vars{
+				"ServiceName": svc.Name,
+			},
+			File("{{ .ServiceName }}.json", Contents(svc)),
+		),
+	)
+}
+
+func serviceName(c *cli.Context) string {
+	name := c.String("name")
+	if name == "" {
+		name = "service"
+		wd, err := os.Getwd()
+		if err == nil {
+			return filepath.Base(wd)
+		}
+	}
+	return name
+}
+
+func service(c *cli.Context) *config.Service {
+	return &config.Service{
+		Title:       c.String("title"),
+		Name:        serviceName(c),
+		Description: c.String("description"),
+		Servers: []*config.Server{
+			{
+				Name:    "default",
+				BaseURL: "http://localhost:8000/",
+			},
+		},
+		Resource: config.Resource{
+			Name: "/",
+			Resources: []config.Resource{
+				{
+					Name: "get",
+					URI:  "/",
+					Get:  &config.Endpoint{},
+				},
+			},
+		},
+	}
+}
