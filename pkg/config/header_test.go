a4d27f0374dd9b96b31ff0c7e69d4536aa648c2c
Gary Mann <garythemann@users.noreply.github.com>
2026-01-06 22:24:05 -0800

    Remove some redundant unit tests

--- pkg/config/header_test.go
+++ pkg/config/header_test.go
@@ -7,7 +7,6 @@ import (
 	"encoding/json"
 
 	"github.com/Carbonfrost/pastiche/pkg/config"
-	"sigs.k8s.io/yaml"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -49,41 +48,6 @@ var _ = Describe("Header", func() {
 				MatchError("unexpected type in config.Header: float64")),
 		)
 	})
-
-	Describe("UnmarshalYAML", func() {
-		DescribeTable("examples",
-			func(yamlString string, expected types.GomegaMatcher) {
-				h := config.Header{}
-				_ = yaml.Unmarshal([]byte(yamlString), &h)
-				Expect(h).To(expected)
-			},
-
-			Entry(
-				"nominal",
-				`Referer: ["https://example.com"]`,
-				Equal(config.Header{"Referer": {"https://example.com"}})),
-
-			Entry(
-				"basic key values",
-				`Referer: https://example.com`,
-				Equal(config.Header{"Referer": {"https://example.com"}})),
-		)
-
-		DescribeTable("errors",
-			func(yamlString string, errExpected types.GomegaMatcher) {
-				h := config.Header{}
-				err := json.Unmarshal([]byte(yamlString), &h)
-				Expect(err).To(errExpected)
-			},
-			Entry(
-				"wrong type", `89`,
-				BeAssignableToTypeOf(&json.UnmarshalTypeError{})),
-			Entry(
-				"error in values",
-				`{"Link":3}`,
-				MatchError("unexpected type in config.Header: float64")),
-		)
-	})
 })
 
 var _ = Describe("Form", func() {
@@ -110,41 +74,6 @@ var _ = Describe("Form", func() {
 			func(jsonString string, errExpected types.GomegaMatcher) {
 				h := config.Form{}
 				err := json.Unmarshal([]byte(jsonString), &h)
-				Expect(err).To(errExpected)
-			},
-			Entry(
-				"wrong type", `89`,
-				BeAssignableToTypeOf(&json.UnmarshalTypeError{})),
-			Entry(
-				"error in values",
-				`{"Link":3}`,
-				MatchError("unexpected type in config.Form: float64")),
-		)
-	})
-
-	Describe("UnmarshalYAML", func() {
-		DescribeTable("examples",
-			func(yamlString string, expected types.GomegaMatcher) {
-				h := config.Form{}
-				_ = yaml.Unmarshal([]byte(yamlString), &h)
-				Expect(h).To(expected)
-			},
-
-			Entry(
-				"nominal",
-				`client_id: ["ci"]`,
-				Equal(config.Form{"client_id": {"ci"}})),
-
-			Entry(
-				"basic key values",
-				`client_id: ci`,
-				Equal(config.Form{"client_id": {"ci"}})),
-		)
-
-		DescribeTable("errors",
-			func(yamlString string, errExpected types.GomegaMatcher) {
-				h := config.Form{}
-				err := json.Unmarshal([]byte(yamlString), &h)
 				Expect(err).To(errExpected)
 			},
 			Entry(
